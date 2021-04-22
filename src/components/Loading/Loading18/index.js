a76601dd984e07530f76d89c2a8e9fe0e89fab2e
Son <sonht1109@gmail.com>
2021-04-23 00:23:31 +0700

    loading 18

---
+++ src/components/Loading/Loading18/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+
+export default function Loading18() {
+  return (
+    <div className="loading-wrapper">
+      <div className="title">Loading 18</div>
+      <div className="loading-18">
+        <div className="dot" />
+      </div>
+    </div>
+  )
+}
--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -8,6 +8,7 @@ import Loading14 from "./Loading14";
 import Loading15 from "./Loading15";
 import Loading16 from "./Loading16";
 import Loading17 from "./Loading17";
+import Loading18 from "./Loading18";
 import Loading2 from "./Loading2";
 import Loading3 from "./Loading3";
 import Loading4 from "./Loading4";
@@ -40,6 +41,7 @@ export default function Loading() {
         <Loading15 />
         <Loading16 />
         <Loading17 />
+        <Loading18 />
       </div>
     </>
   );
